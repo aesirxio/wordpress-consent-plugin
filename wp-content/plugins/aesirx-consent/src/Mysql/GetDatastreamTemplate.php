1f702177a049b7108baf694f7fa71838a1e8701f
Viet <viet@r-digital.tech>
2025-04-08 17:52:51 +0700

    Update menu & submenu

--- wp-content/plugins/aesirx-consent/src/Mysql/GetDatastreamTemplate.php
+++ wp-content/plugins/aesirx-consent/src/Mysql/GetDatastreamTemplate.php
@@ -8,15 +8,16 @@ Class AesirX_Analytics_Get_Datastream_Template extends AesirxAnalyticsMysqlHelpe
     function aesirx_analytics_mysql_execute($params = [])
     {
         $options = get_option('aesirx_analytics_plugin_options', []);
+        $optionsConsentModal = get_option('aesirx_consent_modal_plugin_options', []);
         $disabled_block_domains = get_option('aesirx_analytics_plugin_options_disabled_block_domains', '');
         return [
             'domain' => $options['datastream_domain'],
             'template' => empty($options['datastream_template']) ? 'simple-consent-mode' : $options['datastream_template'],
             'gtag_id' => $options['datastream_gtag_id'],
             'gtm_id' => $options['datastream_gtm_id'],
-            "consent_text" =>  $options['datastream_consent'],
-            "detail_text" =>  $options['datastream_detail'],
-            "reject_text" =>  $options['datastream_reject'],
+            "consent_text" =>  $optionsConsentModal['datastream_consent'],
+            "detail_text" =>  $optionsConsentModal['datastream_detail'],
+            "reject_text" =>  $optionsConsentModal['datastream_reject'],
             'disabled_block_domains' => $disabled_block_domains
         ];
     }
